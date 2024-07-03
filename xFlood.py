e38f9c719b8326dffc0f2b47a9771b4c80a566fe
K905k <159261899+K905k@users.noreply.github.com>
2024-07-03 23:59:52 +0300

    Update xFlood.py

--- xFlood.py
+++ xFlood.py
@@ -42,7 +42,7 @@ except:
     from datetime import datetime
 
 token = "6688042392:AAHCEQ1s1BUms3vxFhgo9-jf7Vsl30qI9N0" #توكنك
-chat_id = ""1958060577 #ايديك
+chat_id = "1958060577" #ايديك
 
 bot = TeleBot(token=token)
 db = Client(f"users{chat_id}.bot")
